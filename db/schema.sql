5621c630a847ea3db959bcd483aa74f2ec8c7b73
Maya Santiago <69778090+msatori@users.noreply.github.com>
2020-11-22 15:56:21 -0600

    all tables created

--- db/schema.sql
+++ db/schema.sql
@@ -9,5 +9,22 @@
     name VARCHAR(30) UNIQUE NOT NULL 
  );
 
+CREATE TABLE role (
+   id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+   title VARCHAR(30) UNIQUE NOT NULL, 
+   salary DECIMAL UNSIGNED NOT NULL,
+   department_id INT UNSIGNED NOT NULL,
+   FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
+CREATE TABLE employees (
+   id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+   first_name VARCHAR(30) NOT NULL,
+   last_name VARCHAR(30) NOT NULL,
+   manager_id INT UNSIGNED NOT NULL, 
+   role_id INT UNSIGNED NOT NULL, 
+   FOREIGN KEY (role_id) REFERENCES role(id),
+   FOREIGN KEY (manager_id) REFERENCES manager(id)
+);
 
 
--- db/seeds.sql
+++ db/seeds.sql
@@ -4,3 +4,42 @@ VALUES
     ('Engineering'),
     ('Legal'),
     ('Finance');
+
+
+INSERT INTO roles (title, salary, department_id)
+VALUES
+    ('Sales Manager', '100000', 1),
+    ('Sales Person', '50000', 1),
+    ('IT Manager', '100000', 2),
+    ('Engineer', '60000', 2),
+    ('Lead Attourney', '200000', 3),
+    ('Attourney', '150000', 3),
+    ('Lead Accountant', '200000', 4),
+    ('Accountant', '90000', 4);
+
+
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES
+  ('Leslie', 'Knope', 1, null),
+  ('Ron', 'Swanson', 1, null),
+  ('Ethel', 'Beavers', 1, null),
+  ('Donna', 'Meagle', 2,  1),
+  ('Ann', 'Perkins', 2,  1),
+  ('Shauna', 'Malwae-Tweep', 2, 1),
+  ('April', 'Ludgate', 2, 1),
+  ('Tammy', 'Two', 3, null),
+  ('Mark', 'Brendanawicz', 3,  null),
+  ('Marcia', 'Langman', 4, 3),
+  ('Liam', 'Bonneville', 4, 3),
+  ('Tom', 'Haverford', 4, 3),
+  ('Mona-Lisa', 'Saperstein', 4, 3),
+  ('Jerry Terry', 'Larry Gary Gergitch', 5, null),
+  ('Chris', 'Traeger', 5, null), 
+  ('Jean-Ralphio', 'Saperstein', 6, 5),
+  ('Bobby', 'Newport', 6, 5),
+  ('Crazy', 'Ira' , 6, 5),
+  ('Benjamin', 'Wyatt', 7, null),
+  ('Andy', 'Dwyer', 8, 7),
+  ('Perd', 'Hapley', 8, 7);
+
