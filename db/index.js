d84163b2e30500a26d6dcace3eada66c089d0676
Maya Santiago <69778090+msatori@users.noreply.github.com>
2020-12-01 15:30:27 -0600

    nearly done with queries

--- db/index.js
+++ db/index.js
@@ -6,8 +6,6 @@ class dataBase {
         this.connection = connection;
     }
 
-
-
 //find all employees - join w/ roles, salaries, departments, and managers
 findAllEmployees () {
     return this.connection.promise().query(
@@ -50,30 +48,63 @@ updateEmployeeManager(employeeId, managerId) {
     );
 }
 
+
+//create new role
+createRole(role) {
+    return this.connection.promise().query(
+        `INSERT INTO role SET ?`,
+        role
+    );
+}
+
+
 //find all roles - join w/ depts
 findAllRoles() {
    return this.connection.promise().query(
        `SELECT role.id, role.title, deparment.name AS department, role.salary FROM role LEFT JOIN department from role.department_id = department.id;`
-   )
+   );
 }
 
-//create new role
-
 //remove a role from the db
+deleteRole(roleId) {
+    return this.connection.promise().query(
+        `DELETE FROM role WHERE id = ?`,
+        roleId
+    );
+}
 
 //find all departments
+findAllDepartments() {
+    return this.connection.promise().query(
+        `SELECT department.id, department.name FROM department;`
+    );
+}
 
 //create a new department 
-
+createDepartment(department) {
+    return this.connection.promise().query(
+        `INSERT INTO department SET ? `,
+        department
+    );
+}
 //remove a department 
+deleteDepartment(departmentId) {
+    return this.connection.promise().query(
+        `DELETE FROM department WHERE id = ?`,
+        departmentId
+    )
+}
 
 //find all employees in a single dept 
+findEmployeeByDepartment(departmentId) {
+    `SELECT employee.id, employee.first_name, department.name AS department, role.title
+     FROM employee 
+     LEFT JOIN role on employee.role_id 
+     LEFT JOIN department on role.department
+     `
+}
 
 //find all employees by manager - join w/ depts and roles
 
 }
 
-connection.query('SELECT * FROM department', function(err, res) {
-    console.log(res);
-
-});
